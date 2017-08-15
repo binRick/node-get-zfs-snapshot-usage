292a8a8dfb0ae8db70ba89b99126eb26c68d674d
root <root@beo.infinitumtech.net>
2017-08-15 12:51:29 -0400

    zfs list async method

--- index.js
+++ index.js
@@ -9,34 +9,47 @@ fs = require('fs'),
     ora = require('ora');
 
 var fileSpinner = ora('Listing ZFS Filesystems...').start();
-var filesystems = child.execSync('zfs list -o name -s usedbysnapshots -pHo name').toString().split('\n').filter(function(l) {
-    return l;
+var fsChild = child.spawn('zfs', ['list', '-oname', '-susedbysnapshots', '-pHoname']);
+var zfsListOut = '';
+fsChild.stdout.on('data', function(data) {
+    zfsListOut += data.toString();
 });
-fileSpinner.succeed('Loaded ' + filesystems.length + ' filesystems..');
+fsChild.stderr.on('data', function(data) {
 
-var countSpinner = ora('Summing Snapshot usage..').start();
-var loaded = 0;
-var totalUsage = 0;
-async.mapSeries(filesystems, function(fs, _cb) {
-    countSpinner.text = '  Queried Snapshot usage on ' + loaded + '/'+filesystems.length+' filesystems. Total usage: ' + totalUsage;
-    loaded++;
-    var snapUsage = 0;
-    var snapChild = child.spawn('zfs', ['get', 'usedbysnapshots', fs, '-pHovalue']);
-    snapChild.on('exit', function(code) {
-        if (code != 0)
-            throw "command failed: " + usageCmd.join(' ');
-        var fsData = {
-            fs: fs,
-            snapUsage: snapUsage
-        };
-        _cb(null, fsData);
+});
+fsChild.on('exit', function(code) {
+    if (code != 0) throw "zfs list failed";
+    var filesystems = zfsListOut.split('\n').filter(function(l) {
+        return l;
     });
-    snapChild.stdout.on('data', function(data) {
-        snapUsage = parseInt(data.toString());
-        totalUsage += snapUsage;
+
+
+    fileSpinner.succeed('Loaded ' + filesystems.length + ' filesystems..');
+
+    var countSpinner = ora('Summing Snapshot usage..').start();
+    var loaded = 0;
+    var totalUsage = 0;
+    async.mapSeries(filesystems, function(fs, _cb) {
+        countSpinner.text = '  Queried Snapshot usage on ' + loaded + '/' + filesystems.length + ' filesystems. Total usage: ' + totalUsage;
+        loaded++;
+        var snapUsage = 0;
+        var snapChild = child.spawn('zfs', ['get', 'usedbysnapshots', fs, '-pHovalue']);
+        snapChild.on('exit', function(code) {
+            if (code != 0)
+                throw "command failed: " + usageCmd.join(' ');
+            var fsData = {
+                fs: fs,
+                snapUsage: snapUsage
+            };
+            _cb(null, fsData);
+        });
+        snapChild.stdout.on('data', function(data) {
+            snapUsage = parseInt(data.toString());
+            totalUsage += snapUsage;
+        });
+        snapChild.stderr.on('data', function(data) {});
+    }, function(errs, totals) {
+        if (errs) throw errs;
+        countSpinner.succeed('Summed ' + loaded + ' filesystems to total of ' + pb(totalUsage));
     });
-    snapChild.stderr.on('data', function(data) {});
-}, function(errs, totals) {
-    if (errs) throw errs;
-    countSpinner.succeed('Summed ' + loaded + ' filesystems to total of ' + pb(totalUsage));
 });
